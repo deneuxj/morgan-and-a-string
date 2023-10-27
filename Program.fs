6bf451b45fa9173ff9cf05c2583da0f0f0e3d726
johann deneux <johann.deneux@gmail.com>
2023-10-27 18:41:33 +0200

    Add again slow method
    It produces results that are correct, is optimized enough to avoid stack overflows on the test cases, but does not terminated within the time limit on all test cases.
    
    Its main benefit is to allow investigate why the eager method does not work by making it possible to compare the two.

--- Program.fs
+++ Program.fs
@@ -1,5 +1,35 @@
-﻿
+﻿let cmpCharSeqs (seqA : char seq, seqB : char seq) =
+    let it1 = seqA.GetEnumerator()
+    let it2 = seqB.GetEnumerator()
+    let rec work() =
+        let hasNext1 = it1.MoveNext()
+        let hasNext2 = it2.MoveNext()
+        if hasNext1 && hasNext2 then
+            let a = it1.Current
+            let b = it2.Current
+            if a < b then -1
+            elif a > b then 1
+            else work()
+        elif hasNext1 then
+            1
+        elif hasNext2 then
+            -1
+        else
+            0
+    work()
+
 let morganAndString (lineA : string, lineB : string) =
+    let rec commonConstantPrefixLength c (idxA, idxB) n =
+        if idxA < lineA.Length && idxB < lineB.Length then
+            let a = lineA.[idxA]
+            let b = lineB.[idxB]
+            if a = b && b = c then
+                commonConstantPrefixLength c (idxA + 1, idxB + 1) (n + 1)
+            else
+                n
+        else
+            n
+
     let rec skipEquals eqChar (idxA, idxB) n =
         if idxA < lineA.Length && idxB < lineB.Length then
             let a = lineA.[idxA]
@@ -52,11 +82,25 @@ let morganAndString (lineA : string, lineB : string) =
                     yield! work (idxA, idxB + 1)
                 }
             else
-                let advA, advB = skipEquals a (idxA + 1, idxB + 1) 1
-                seq {
-                    yield! Seq.init (advA + advB) (fun _ -> a)
-                    yield! work (idxA + advA, idxB + advB)
-                }
+                let skipLen = commonConstantPrefixLength a (idxA, idxB) 0
+                let seqA = work (idxA + skipLen, idxB)
+                let seqB = work (idxA, idxB + skipLen)
+                let cmp = cmpCharSeqs (seqA, seqB)
+                if cmp < 0 then
+                    seq {
+                        yield! Seq.replicate skipLen a
+                        yield! seqA
+                    }
+                else
+                    seq {
+                        yield! Seq.replicate skipLen b
+                        yield! seqB
+                    }
+                // let advA, advB = skipEquals a (idxA + 1, idxB + 1) 1
+                // seq {
+                //     yield! Seq.init (advA + advB) (fun _ -> a)
+                //     yield! work (idxA + advA, idxB + advB)
+                // }
         elif idxA < lineA.Length then
             lineA.[idxA..] :> char seq
         else
